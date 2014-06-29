eab68224e4af71881f975bc401c9c7e04e9445d7
Ironpinguin <michele@catalano.de>
2014-06-29 19:41:01 +0000

    change config elasticesearch url with extra index and doc type parameter

--- outputs/elasticsearchindex.py
+++ outputs/elasticsearchindex.py
@@ -4,11 +4,12 @@ import json
 from datetime import datetime
 
 class ElasticsearchIndex(output.Output):
-	requiredData = ["Url","Index"]
+	requiredData = ["Url","Index","Type"]
 	optionalData = []
 	def __init__(self,data):
 		self.Url=data["Url"]
 		self.Index=data["Index"]
+                self.Type=data["Type"]
 	def outputData(self,dataPoints):
 		arr = {}
 		for i in dataPoints:
@@ -16,7 +17,7 @@ class ElasticsearchIndex(output.Output):
                 arr['messure_date'] = datetime.utcnow().isoformat() + 'Z'
 		a = json.dumps(arr)
 		try:
-			z = requests.post(self.Url,data=a)
+			z = requests.post(self.Url + "/" + self.Index + "/" + self.Type,data=a)
                         response = json.loads(z.text)
 			if response[u'created'] != True: 
 				print "Elasticsearch Error: " + response
